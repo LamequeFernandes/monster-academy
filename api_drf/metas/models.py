3f3e028820727dc89e774bb85d3253e4eff2dc29
LamequeFernandes <lamequesao@gmail.com>
2022-09-28 11:11:31 -0300

    criacao de app metas e model

---
+++ api_drf/metas/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from core.base_model import BaseModel
+
+from usuario.models import Usuario
+
+
+class Meta(BaseModel):
+    titulo = models.CharField(max_length=30)
+    descricao = models.TextField(max_length=500)
+    cpf_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
