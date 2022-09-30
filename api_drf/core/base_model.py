85b0aa68930b242f6e4cfa974be6db4430bc7384
LamequeFernandes <lamequesao@gmail.com>
2022-09-30 12:32:54 -0300

    correcao em baseModel e refazendo migrates

--- api_drf/core/base_model.py
+++ api_drf/core/base_model.py
@@ -8,3 +8,6 @@ class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     dt_criacao = models.DateTimeField(auto_now_add=True)
     dt_update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract=True
--- api_drf/exercicio/urls.py
+++ api_drf/exercicio/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path, include
-from rest_framework import routers
-from . import views
-
-
-router = routers.DefaultRouter()
-router.register(r'exercicios', viewset=views.ExercicioViewSet)
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
+# from django.urls import path, include
+# from rest_framework import routers
+# from . import views
+#
+#
+# router = routers.DefaultRouter()
+# router.register('exercicios', viewset=views.ExercicioViewSet)
+#
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
