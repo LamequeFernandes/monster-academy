57c8b9e3e89df71dceeeb6c181f2ce70f758c518
LamequeFernandes <lamequesao@gmail.com>
2022-09-30 14:59:51 -0300

    alteracoes nas urls

--- api_drf/api/urls.py
+++ api_drf/api/urls.py
@@ -15,15 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import  routers
 
-from exercicio.views import ExercicioViewSet
-
-
-router = routers.DefaultRouter()
-router.register(r'exercicio', ExercicioViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('', include('exercicio.urls')),
 ]
